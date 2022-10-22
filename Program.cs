0f13c443b8694821e7a0f6f480424f6892a2eb03
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-22 14:59:58 +0530

    checkfor

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+namespace AbstractClass
+{
+    abstract class MotorBike
+    {
+
+        public abstract void brake();
+    }
+
+    class SportsBike : MotorBike
+    {
+
+        
+        public override void brake()
+        {
+            Console.WriteLine("Sports Bike Brake");
+        }
+
+    }
+    class MountainBike : MotorBike
+    {
+
+        
+        public override void brake()
+        {
+            Console.WriteLine("Mountain Bike Brake");
+        }
+
+    }
+}
