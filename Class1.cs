8329ac579b727b2929f9672fbee568ac7d125fa8
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-22 15:05:04 +0530

    checkfor

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿
+
+using AbstractClass;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        SportsBike s1 = new SportsBike();
+        s1.brake();
+
+        
+        AbstractClass.MountainBike m1 = new MountainBike();
+        m1.brake();
+
+        Console.ReadLine();
+    }
+}
